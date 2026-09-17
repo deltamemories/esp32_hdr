50cd6c062ba20c14e96f2859141f4edb89d232aa
deltamemories <maxenot123@gmail.com>
2026-09-17 20:14:14 +0200

    feat(esp32): setup TFT display with test message

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,20 +1,52 @@
 #include <Preferences.h>
+
 #include "esp_camera.h"
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
 
 Preferences preferences;
 camera_config_t config;
+
+#define TFT_CS 10
+#define TFT_DC 13
+#define TFT_RST 14
+#define TFT_MOSI 11
+#define TFT_SCLK 12
+
+// screen dimensions
+#define SW 240
+#define SH 320
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+GFXcanvas16 canvas(SW, SH);
 
 void setup() {
   Serial.begin(115200);
   preferences.begin("config", false);
 
+  // TFT init
+  SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
+  tft.init(240, 320);
+  tft.setSPISpeed(80'000'000);
+  tft.invertDisplay(false);
+  tft.setRotation(0);
+  // TFT init end
+
   if (init_camera() != ESP_OK) {
     Serial.println("camera init failed");
   }
+
+  canvas.fillScreen(ST77XX_BLACK);
+  canvas.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  canvas.setTextSize(2);
+  canvas.println("test");
+  
+  tft.drawRGBBitmap(0, 0, canvas.getBuffer(), SW, SH);
 }
 
-void loop(){
+void loop() {
   camera_fb_t *fb = esp_camera_fb_get();
   if (!fb) {
     return;
